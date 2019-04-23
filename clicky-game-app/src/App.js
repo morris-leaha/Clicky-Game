a93eaba90e7976827d15373e08c2bbb58f3560a3
LAMorris <morris.leaha@gmail.com>
2019-04-23 01:38:07 -0500

    [App.js] added Navbar component import to file

--- clicky-game-app/src/App.js
+++ clicky-game-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 class App extends Component {
   render() {
