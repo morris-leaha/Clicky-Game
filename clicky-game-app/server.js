30f086ef40b8ffabe65ddc18f1eaa92f11b30977
LAMorris <morris.leaha@gmail.com>
2019-04-24 11:21:47 -0500

    [server.js] changed static folder to serve static content for heroku

--- clicky-game-app/server.js
+++ clicky-game-app/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("public"));
+    app.use(express.static("build"));
 }
 
 // ============= LISTENER INFO ==============
